f82c86b73f45abe458eacc8e3275dc07b55b620c
Jasper Hemmes <jghemmes@gmail.com>
2021-05-17 14:26:11 +0200

    first commit of branch

---
+++ dsr/turbulence/resultsprocessing.py
@@ -0,0 +1,18 @@
+# Jasper Hemmes - 2021
+
+import os
+import sys
+import pickle
+import numpy as np
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+
+
+if __name__ == "__main__":
+
+
+
+    print('end')
