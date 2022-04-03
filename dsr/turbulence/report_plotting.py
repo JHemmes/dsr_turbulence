0afa63f152c3a80024c0ec46aec73ed88f5bacfc
Jasper Hemmes <78410021+JHemmes@users.noreply.github.com>
2022-04-03 22:26:27 +0200

    added function that makes plots for report

--- dsr/turbulence/report_plotting.py
+++ dsr/turbulence/report_plotting.py
@@ -1,11 +1,12 @@
 import os
 import platform
 import numpy as np
+import json
 
-from resultprocessing import load_iterations
+from dsr.turbulence.resultprocessing import fetch_iteration_metrics, compare_dicts, report_plot
 
 import matplotlib
-matplotlib.use('tkagg')
+matplotlib.use('PS')
 import matplotlib.pyplot as plt
 
 def create_plots_for_increasing_n_iterations():
@@ -129,6 +130,148 @@ def contourplot_vars():
     # plt.contourf(mesh_x, mesh_y, , levels=30, vmin=ymin, vmax=ymax, cmap='Reds')
     plt.colorbar()
 
+def plot_pretty_sensitivity_results(logdir, parameters):
+    # first scan dirs, save required dirs
+    # depending on plots required, fetch required iterations and plot accordingly
+
+    dirlist = os.listdir(logdir)
+    dirlist.remove('config_baseline.json')
+    try:
+        dirlist.remove('results')
+    except ValueError:
+        pass
+
+    try:
+        dirlist.remove('report_plots')
+    except ValueError:
+        pass
+
+    try:
+        os.mkdir(os.path.join(logdir, 'report_plots'))
+    except FileExistsError:
+        pass
+
+
+    with open(os.path.join(logdir, 'config_baseline.json'), encoding='utf-8') as f:
+        config_bsl = json.load(f)
+
+    bsl_params = {
+        'entropy_weight': config_bsl['controller']['entropy_weight'],
+        'learning_rate': config_bsl['controller']['learning_rate'],
+        'initializer': config_bsl['controller']['initializer'],
+        'num_layers': config_bsl['controller']['num_layers'],
+        'num_units': config_bsl['controller']['num_units']
+    }
+
+    base_labels_params = {
+        'entropy_weight': (lambda x: f'$\lambda_H = {x}$'),
+        'learning_rate': (lambda x: f'$\\alpha = {x}$'),
+        'initializer': (lambda x: f'Initialiser: {x}'),
+        'num_layers': (lambda x: f'$n_{{layers}} = {x}$'),
+        'num_units': (lambda x: f'$n_{{units}} = {x}$')
+    }
+
+    results = {param: {} for param in parameters}
+
+    for run in dirlist:
+        run_dir = os.path.join(logdir, run)
+        with open(os.path.join(run_dir, 'config.json'), encoding='utf-8') as f:
+            config_run = json.load(f)
+
+        diff = compare_dicts(config_bsl, config_run)
+
+        if diff[0] == 'baseline':
+            results['baseline'] = {'data': fetch_iteration_metrics(run_dir)}
+            print(run_dir)
+        else:
+            if len(diff) == 1:
+                if diff[0][0] in parameters:
+                    results[diff[0][0]][run_dir] = {'value': diff[0][1]}
+                if diff[0][0] == 'initializer':
+                    # also figure out the standard deviation
+                    results[diff[0][0]][run_dir] = {'value': f"${config_run['task']['name'].split('_')[-1]}$"}
+
+
+
+    for param in parameters:
+        results[param]['par_val'] = [bsl_params[param]]
+
+        # write def that return the max given the data/load_iterations result
+        tmp_arr = np.array(results['baseline']['data']['base_r_best'])
+        results[param]['arrays'] = [np.max(tmp_arr, axis=0)]
+        for run in results[param]:
+            if 'log' in run:
+                results[param]['par_val'].append(results[param][run]['value'])
+                data = fetch_iteration_metrics(run)
+
+                tmp_arr = np.array(data['base_r_best'])
+                results[param]['arrays'].append(np.max(tmp_arr, axis=0))
+                del tmp_arr
+
+        # create requirements for plot:
+        # legend needs to be sorted, baseline always is C0.
+        x = np.arange(results[param]['arrays'][0].shape[0])
+        y = []
+        labels = []
+        colors = [f'C{ii}' for ii in range(len(results[param]['par_val']))]
+        sorted_colors = []
+        for ii in np.argsort(results[param]['par_val']):
+            y.append(results[param]['arrays'][ii])
+            sorted_colors.append(colors[ii])
+            labels.append(base_labels_params[param](results[param]['par_val'][ii]))
+            if results[param]['par_val'][ii] == bsl_params[param]:
+                labels[-1] = labels[-1] + ' (BSL)'
+
+        filename = os.path.join(logdir, 'report_plots', f'{logdir.split("_")[-1]}_{param}.eps')
+
+        ########################## below this only bDelta double plot
+        # param = 'num_units' (if the first param is the other one)
+        # results[param]['par_val'] = [bsl_params[param]]
+        #
+        # # write def that return the max given the data/load_iterations result
+        # tmp_arr = np.array(results['baseline']['data']['base_r_best'])
+        # results[param]['arrays'] = [np.max(tmp_arr, axis=0)]
+        # for run in results[param]:
+        #     if 'log' in run:
+        #         results[param]['par_val'].append(results[param][run]['value'])
+        #         data = fetch_iteration_metrics(run)
+        #
+        #         tmp_arr = np.array(data['base_r_best'])
+        #         results[param]['arrays'].append(np.max(tmp_arr, axis=0))
+        #         del tmp_arr
+        #
+        # # create requirements for plot:
+        # # legend needs to be sorted, baseline always is C0.
+        # x = np.arange(results[param]['arrays'][0].shape[0])
+        # # y = []
+        # # labels = []
+        # colors = [f'C{ii}' for ii in range(len(results[param]['par_val']))]
+        # # sorted_colors = []
+        # for ii in np.argsort(results[param]['par_val']):
+        #     y.append(results[param]['arrays'][ii])
+        #     sorted_colors.append(colors[ii])
+        #     labels.append(base_labels_params[param](results[param]['par_val'][ii]))
+        #     if results[param]['par_val'][ii] == bsl_params[param]:
+        #         labels[-1] = labels[-1] + ' (BSL)'
+        #
+        # sorted_colors[-1] = 'C3'
+        #
+        # filename = os.path.join(logdir, 'report_plots', 'bDeltaCombined.eps')
+        ######################### above only for bDelta double plot
+
+        figsize = (12, 9)
+        xlabel = 'Iterations'
+        ylabel = r'$r_{max}(\tau)$'
+
+        report_plot(x, y, labels, sorted_colors, xlabel, ylabel, filename, figsize)
+
+    # make the combined bDelta plot:
+
+
+
+
+
+    print('here')
 
 
 if __name__ == "__main__":
@@ -141,7 +284,12 @@ if __name__ == "__main__":
 
 
     # create_plots_for_increasing_n_iterations()
+    # logdir = '../logs_completed/compare_iterlim_optimisation'
+    # logdir = '../logs_completed/sensitivity_analysis_kDeficit'
+    logdir = '../logs_completed/sensitivity_analysis_bDelta'
 
+    plot_pretty_sensitivity_results(logdir, ['learning_rate', 'num_units'])
+    # plot_pretty_sensitivity_results(logdir, ['initializer'])
 
     print('end')
     print('end')
